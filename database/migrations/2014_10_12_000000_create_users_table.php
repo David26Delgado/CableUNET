001e3e4a356fa21eac08e052fa0ec72d7611b0a2
David26Delgado <dadch13.14@gmail.com>
2018-11-19 12:18:46 -0400

    Creados login y registro

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_admin')->default(false);
             $table->string('password');
+            $table->integer('tipo')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
