1f98a83e076ff096dcf0c9f2c905842a9d735d34
David26Delgado <dadch13.14@gmail.com>
2018-11-20 21:21:08 -0400

    Arreglada vista del Dashboard User

--- resources/views/user/userdash.blade.php
+++ resources/views/user/userdash.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.app')
 
+@section('header', "Dashboard")
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Esta es una sesión de usuario. ¡Bienvenido!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    Esta es una sesión de usuario. ¡Bienvenido!
+    
 @endsection
