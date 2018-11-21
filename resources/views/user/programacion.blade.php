ec093d4d94809ae6ec340bc436b074dcb3f3aad8
oriana0anairo <orianamendezprato@hotmail.com>
2018-11-21 00:34:36 -0400

    min hora

--- resources/views/user/programacion.blade.php
+++ resources/views/user/programacion.blade.php
@@ -8,6 +8,21 @@
     <option value="1">One</option>
     <option value="2">Two</option>
     <option value="3">Three</option>
+    <br>
+</select>
+<br>
+<select class="custom-select">
+    <option selected>Hora</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+</select>
+<br>
+<select class="custom-select">
+    <option selected>min</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
 </select>
 
        
